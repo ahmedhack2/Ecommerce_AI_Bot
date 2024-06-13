8e7259e76469e99ca56973d4ef62c0f01d05885e
ahmedhack2 <ahmedmadelkazafy@gmail.com>
2024-06-14 02:56:41 +0300

    app.py

--- app.py
+++ app.py
@@ -4,10 +4,5 @@ from resources import ns
 
 def create_app():
     app = Flask(__name__)
-
-    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-    # api.init_app(app)
-    # db.init_app(app)
-
     api.add_namespace(ns)
     return app
